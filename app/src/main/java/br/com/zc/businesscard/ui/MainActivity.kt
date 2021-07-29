5b737ee8f738425918b93b5cf19a5e49d838e78d
Zenilton <zenilton.cardoso@unifebe.edu.br>
2021-07-29 10:23:18 -0300

    feat: Criação de layout de eventos e navegação de telas

--- app/src/main/java/br/com/zc/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/zc/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.zc.businesscard.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.zc.businesscard.databinding.ActivityMainBinding
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insetListener()
+    }
+
+    private fun insetListener() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
